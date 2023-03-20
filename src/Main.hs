8a1bfb8987a9d4a2b4e3083d8f8e41a63e170cc9
Carter Ison <carter@isons.org>
2023-03-20 15:45:35 -0700

    Implement rudimentary rendering from internal rep

--- src/Main.hs
+++ src/Main.hs
@@ -16,4 +16,29 @@
  -  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  -}
 
-main = putStrLn "Hello, World!"
+import Data.Array.Unboxed
+
+type Bitmap = Array (Int, Int) Bool
+
+testBM :: Bitmap
+testBM = listArray ((0, 0), (8, 8)) (True : False : False : True : repeat False)
+
+main = do
+  mapM (putStrLn . draw . uncurry zip) . thing $ getRows testBM
+
+getRows :: Bitmap -> [[Bool]]
+getRows fa = [[fa ! (i, j) | i <- [0..xb]] | j <- [0..yb]]
+  where (_, (xb, yb)) = bounds fa
+
+thing :: [[a]] -> [([a], [a])]
+thing [] = [([], [])]
+thing [a] = [(a, [])]
+thing [a, b] = [(a, b)]
+thing (a : b : xs) = (a, b) : thing xs
+
+draw :: [(Bool, Bool)] -> String
+draw [] = ""
+draw ((False, False) : xs) = ' ' : draw xs
+draw ((False, True) : xs) = '\x2584' : draw xs
+draw ((True, False) : xs) = '\x2580' : draw xs
+draw ((True, True) : xs) = '\x2588' : draw xs
